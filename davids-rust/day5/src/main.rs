f25f4c3a150b9bbcf10ba6afca5d092b8b1f8c84
David <david.smitmanis@gmail.com>
2016-12-11 22:17:05 +0100

    Day 5 part 2

--- davids-rust/day5/src/main.rs
+++ davids-rust/day5/src/main.rs
@@ -1,6 +1,7 @@
 extern crate crypto;
 use crypto::md5::Md5;
 use crypto::digest::Digest;
+use std::io::{self, Write};
 
 struct Md5Hasher {
     hasher: Md5
@@ -39,18 +40,64 @@ fn find_password(input: &str, password_length: usize) -> String {
     password
 }
 
+fn find_password_v2(input: &str, password_length: usize) -> String {
+    let mut hasher = Md5Hasher::new();
+    let mut password_chars: Vec<Option<char>> = vec![None; password_length];
+    let mut test_input = input.to_string();
+    let mut counter = 0u64;
+    let mut chars_filled = 0;
+    while chars_filled < password_length {
+        test_input.push_str(&counter.to_string());
+        let hash = hasher.md5sum(&test_input);
+        if hash.starts_with("00000") {
+            let mut hash_chars = hash.chars().skip(5);
+            if let Some(p) = hash_chars.next().unwrap().to_digit(10) {
+                let pos = p as usize;
+                if pos < password_length && password_chars[pos] == None {
+                    password_chars[pos] = Some(hash_chars.next().unwrap());
+                    chars_filled += 1;
+
+                    // Very cinematic
+                    print!("{}\r", password_chars.iter()
+                             .map(|&o| match o {
+                                 Some(c) => c,
+                                 None => ' '
+                             })
+                           .collect::<String>());
+                    io::stdout().flush().unwrap();
+                }
+            }
+        }
+        test_input.truncate(input.len());
+        counter += 1;
+    }
+
+    password_chars.iter()
+        .map(|o| o.unwrap())
+        .collect()
+}
+
 fn main() {
     let input = "cxdnnyjw";
+    println!("Part 1");
     println!("Password: {}", find_password(input, 8));
+    println!("Part 2");
+    println!("Password: {}", find_password_v2(input, 8));
 }
 
 #[cfg(test)]
 mod test {
-    use super::find_password;
+    use super::{find_password, find_password_v2};
 
     #[test]
     fn tc_1() {
         let input = "abc";
         assert_eq!(find_password(input, 8), "18f47a30".to_string());
+    }
+
+    #[test]
+    fn tc_2() {
+        let input = "abc";
+        assert_eq!(find_password_v2(input, 8), "05ace8e3".to_string());
     }
 }
