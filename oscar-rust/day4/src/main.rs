66d85b68b12173fbaaf7d3331c0e67eeca1ac989
Oscar Carlsson <oscar.carlsson@gmail.com>
2017-02-04 18:54:29 +0100

    Day 4 part 2 / jag kan marginellt mer rust nu
    Huvudsakligen en upprensning av den tidigare lösningen.

--- oscar-rust/day4/src/main.rs
+++ oscar-rust/day4/src/main.rs
@@ -1,122 +1,95 @@
 #[macro_use] extern crate lazy_static;
-#[macro_use] extern crate itertools;
 extern crate regex;
 
 use regex::Regex;
-use std::fmt;
-use itertools::Itertools;
 use std::io::{BufReader, BufRead};
 use std::path::Path;
 use std::collections::HashMap;
 use std::cmp::Ordering;
+use std::fs::File;
 
 lazy_static! {
     static ref CHECKSUM_SPLIT:Regex = Regex::new("(?x)
 (?P<payload>[a-z-]+)-
-(?P<numbers>[0-9]+)
+(?P<shift>[0-9]+)
 \\[(?P<checksum>[a-z]+)\\]").unwrap();
 }
 
-struct CodeParts {
-    payload: Vec<char>,
-    checksum: Vec<char>,
-    numbers: i32,
+#[derive(Debug)]
+struct CodeParts<'a> {
+    payload: Vec<&'a str>,
+    checksum: Vec<&'a str>,
+    shift: i32,
+    calc_checksum: Vec<&'a str> //HashMap<&'a str, i32>
 }
 
-impl CodeParts {
-    fn new<S>(room_code: S) -> CodeParts where S: Into<String> {
-        let _room: &str = &room_code.into();
-        let caps = CHECKSUM_SPLIT.captures(_room).unwrap();
-        let payload: &str = &caps["payload"];
-        let mut checksum: Vec<char> = caps["checksum"].chars().collect();
-        checksum.sort_by(|a, b| a.cmp(&b));
+impl<'a> CodeParts<'a> {
+    fn new(room_code: &str) -> CodeParts {
+        let caps: Vec<_> = CHECKSUM_SPLIT.captures(room_code).into_iter().collect();
+
+        let payload: Vec<&str> = caps[0].name("payload")
+            .expect("No payload matched")
+            .as_str()
+            .split("-")
+            .collect();
+
+        let shift: i32 = caps[0].name("shift")
+            .expect("No shift matched")
+            .as_str()
+            .parse()
+            .expect("Could not parse shift as digit");
+
+
+        let checksum: Vec<&str> = caps[0].name("checksum")
+            .expect("No checksum match")
+            .as_str()
+            .split("")
+            .filter(|c| !c.is_empty())
+            .take(5)
+            .collect();
+
+        let mut occurences: HashMap<&str, u32> = HashMap::new();
+        for part in &payload {
+            let parts: Vec<&str> = part.split("").filter(|p| !p.is_empty()).collect();
+            for letter in &parts {
+                *occurences.entry(letter).or_insert(0) += 1;
+            }
+        }
+        let calc_checksum = calculate_checksum(occurences);
 
         CodeParts {
-            payload: payload.chars().filter(|a| a.is_alphanumeric()).collect::<Vec<char>>(),
+            payload: payload,
+            shift: shift,
             checksum: checksum,
-            numbers: caps["numbers"].parse::<i32>().unwrap(),
+            calc_checksum: calc_checksum
         }
+    }
+
+    fn is_valid(&self) -> bool {
+        self.calc_checksum == self.checksum
+    }
+
+    fn unshift(&self) -> String {
+        let shift: u8 = (self.shift % 26) as u8;
+        let case = 'a' as u8;
+
+        // Det här måste gå att skriva mer lättförståerligt utan att
+        // behöva skapa mellansträngen...
+        self.payload.iter().map(|part| {
+            part.chars().map(|c| {
+                (((c as u8 - case + shift) % 26) + case) as char
+            }).collect::<String>()
+        }).collect::<Vec<String>>().join(" ")
     }
 }
 
-impl fmt::Debug for CodeParts {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f,
-               "CodeParts{{payload: {p}, checksum: {c}, numbers: {n}}}",
-               p = self.payload.iter().join(""),
-               c = self.checksum.iter().join(""),
-               n = self.numbers)
-    }
-}
 
-fn main() {
-    let valids = ["aaaaa-bbb-z-y-x-123[abxyz]", // true
-                  "a-b-c-d-e-f-g-h-987[abcde]", // true
-                  "not-a-real-room-404[oarel]", // true
-                  "totally-real-room-200[decoy]" // false
-    ];
+fn calculate_checksum<'a>(occurences: HashMap<&'a str, u32>) -> Vec<&'a str> {
+    let mut count_vec: Vec<_> = occurences
+        .iter()
+        .collect();
 
-    let mut index = 0;
-    let mut num_correct = 0;
-    let mut total_sum = 0;
-    let size = valids.len();
-    loop {
-        let parts = CodeParts::new(valids[index]);
-        let checksum_ok: bool = control_checksum2(&parts);
-
-        if checksum_ok {
-            num_correct += 1;
-            total_sum += parts.numbers;
-        }
-
-        index += 1;
-        if index == size {
-            break;
-        }
-    }
-
-    assert!(num_correct == 3, "The number of correct test data is 3, not {}.", num_correct);
-    assert!(total_sum == 1514, "The total sum id isn't correct ({} != 1514)", total_sum);
-
-    let path = Path::new("src/input.txt");
-    if !path.exists() {
-        panic!("Input path does not exist! {}", path.display());
-    }
-
-    let input_file = match std::fs::File::open(path) {
-        Ok(file) => file,
-        Err(e) => {
-            panic!("Could not open input.txt! {}", e);
-        }
-    };
-
-    let bufreader = BufReader::new(&input_file);
-    let mut num_correct = 0;
-    let mut total_sum = 0;
-    for line in bufreader.lines() {
-        let parts = CodeParts::new(line.unwrap());
-        let checksum_correct: bool = control_checksum2(&parts);
-
-        if checksum_correct {
-            num_correct += 1;
-            total_sum += parts.numbers;
-        }
-    }
-
-    println!("num_correct: {}", num_correct);
-    println!("total sum: {}", total_sum);
-
-}
-
-fn control_checksum2(parts: &CodeParts) -> bool {
-    // http://stackoverflow.com/questions/34555837/sort-hashmap-data-by-value
-    let mut count: HashMap<char, u32> = HashMap::new();
-
-    for letter in &parts.payload {
-        *count.entry(*letter).or_insert(0) += 1;
-    }
-
-    let mut count_vec: Vec<_> = count.iter().collect();
+    // Sortera först på antalet, om lika på bokstaven
     count_vec.sort_by(|a, b| {
         match a.1.cmp(b.1).reverse() {
             Ordering::Equal => a.0.cmp(b.0),
@@ -124,26 +97,65 @@ fn control_checksum2(parts: &CodeParts) -> bool {
         }
     });
 
-    let mut calculated_checksum = count_vec[0..5]
+    // Kasta siffrorna och ta bara de fem första
+    count_vec
         .iter()
-        .map(|&(letter, _)| letter)
-        .collect::<Vec<&char>>();
-    calculated_checksum.sort();
+        .map(|&(letter, _)| *letter)
+        .take(5)
+        .collect::<Vec<&str>>()
+}
 
+fn part1() {
+    // let valids = ["aaaaa-bbb-z-y-x-123[abxyz]", // true
+    //               "a-b-c-d-e-f-g-h-987[abcde]", // true
+    //               "not-a-real-room-404[oarel]", // true
+    //               "totally-real-room-200[decoy]" // false
+    // ];
 
-    let actual_checksum = &parts.checksum;
+    let bufreader = get_lines();
+    let mut sector_sum = 0;
+    let mut north_pole = 0;
 
-    // http://stackoverflow.com/a/29504547/285103
-    let matching = calculated_checksum
-        .iter()
-        .zip(actual_checksum.iter())
-        .filter(|&(calculated_checksum, actual_checksum)|
-                *calculated_checksum == actual_checksum)
-        .count();
+    for bufline in bufreader.lines() {
+        let line = bufline.expect("Something wrong with this line");
+        let codeparts = CodeParts::new(&line);
 
-    if matching == 5 {
-        true
-    } else {
-        false
+        if codeparts.is_valid() {
+            sector_sum += codeparts.shift
+        }
+
+        // What is the sector ID of the room where North Pole objects
+        // are stored?
+        let unshifted = codeparts.unshift();
+        if unshifted.contains("north") {
+            north_pole = codeparts.shift
+        }
     }
+
+    // What is the sum of the sector IDs of the real rooms?
+    assert_eq!(sector_sum, 185371);
+    println!("Sector sum: {}", sector_sum);
+    println!("Stuff stored in: {}", north_pole);
+}
+
+fn part2() {
+    let encrypted = CodeParts::new("qzmt-zixmtkozy-ivhz-343[zimth]");
+    assert_eq!(encrypted.unshift(), "very encrypted name");
+}
+
+fn main() {
+    part1();
+    part2();
+}
+
+#[allow(dead_code)]
+fn get_lines() -> BufReader<File> {
+    let path = Path::new("src/input.txt");
+    if !path.exists() {
+        panic!("Input path does not exist! {}", path.display());
+    }
+
+    let input_file = File::open(path).expect("Could not open input.txt!");
+
+    BufReader::new(input_file)
 }
