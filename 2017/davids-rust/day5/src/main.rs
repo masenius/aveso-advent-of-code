7ff7826f94081371a6238565dcc26ed65d3e9466
David <david.smitmanis@gmail.com>
2017-12-09 17:15:19 +0100

    Lesson of the day: Immutable is good until it leads you to overengineered solutions...

--- 2017/davids-rust/day5/src/main.rs
+++ 2017/davids-rust/day5/src/main.rs
@@ -1,21 +1,16 @@
-use std::collections::HashMap;
-
-fn part_1(instructions: &[isize]) -> u32 {
+fn part_1(instructions: &mut [isize]) -> u32 {
     let mut i = 0;
     let mut steps = 0;
-    let mut offsets = HashMap::new();
-    while let Some(num) = instructions.get(i) {
-        let offset = offsets.entry(i).or_insert(0);
-        i = i + *offset;
-        *offset += 1;
+    while let Some(num) = instructions.get_mut(i) {
         steps += 1;
 
-        let signed_i = i as isize + num;
+        let signed_i = i as isize + *num;
         if signed_i < 0 {
             break;
         }
 
         i = signed_i as usize;
+        *num += 1;
     }
     steps
 }
@@ -26,5 +21,5 @@ fn main() {
         .filter_map(|l| l.parse().ok())
         .collect::<Vec<isize>>();
 
-    println!("Part 1: Number of steps: {}", part_1(&instructions));
+    println!("Part 1: Number of steps: {}", part_1(&mut instructions.clone()));
 }
