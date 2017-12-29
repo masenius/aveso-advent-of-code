83c32bbc6196ebaf742d335e946fabc2a19ee152
David <david.smitmanis@gmail.com>
2017-12-29 14:26:57 +0100

    Day 10 part 2

--- 2017/davids-rust/day10/src/main.rs
+++ 2017/davids-rust/day10/src/main.rs
@@ -7,25 +7,45 @@ fn reverse_section(list: &mut [u16], start: usize, length: usize) {
     }
 }
 
-fn part1(list: &mut [u16], lengths: &[usize]) -> u16 {
+fn hash(list: &mut [u16], lengths: &[usize], rounds: usize) {
     let mut skip_size = 0;
     let mut pos = 0;
-    for length in lengths.iter() {
-        reverse_section(list, pos, *length);
-        pos += length + skip_size;
-        pos = pos % list.len();
-        skip_size += 1;
+    for _ in 0..rounds {
+        for length in lengths.iter() {
+            reverse_section(list, pos, *length);
+            pos += length + skip_size;
+            pos = pos % list.len();
+            skip_size += 1;
+        }
     }
+}
+
+fn hash_v1(list: &mut [u16], lengths: &[usize]) -> u16 {
+    hash(list, lengths, 1);
     list[0] * list[1]
+}
+
+fn hash_v2(list: &mut [u16], lengths: &[usize]) -> String {
+    hash(list, lengths, 64);
+    list.chunks(16)
+        .map(|c| c.iter().fold(0, |res, &num| res ^ num))
+        .map(|n| format!("{:02x}", n))
+        .collect::<String>()
 }
 
 fn main() {
     let input = include_str!("input");
-    let lengths = input.split(",")
+    let lengths_v1 = input.split(",")
         .filter_map(|n| n.parse().ok())
         .collect::<Vec<usize>>();
     let list = (0..256).collect::<Vec<u16>>();
-    println!("Part 1: Result: {}", part1(list.clone().as_mut_slice(), &lengths));
+    println!("Part 1: Result: {}", hash_v1(list.clone().as_mut_slice(), &lengths_v1));
+    let mut lengths_v2 = input.chars()
+        .filter(|c| !c.is_whitespace())
+        .map(|c| c as usize)
+        .collect::<Vec<usize>>();
+    lengths_v2.extend_from_slice(&[17, 31, 73, 47, 23]);
+    println!("Part 2: Result: {}", hash_v2(list.clone().as_mut_slice(), &lengths_v2));
 }
 
 #[cfg(test)]
