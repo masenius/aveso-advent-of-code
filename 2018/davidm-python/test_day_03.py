e34d1b0d46d97b976470baba294e3cd19f062d08
David Mansson <masenius@gmail.com>
2018-12-09 14:25:13 +0100

    Formatting

--- 2018/davidm-python/test_day_03.py
+++ 2018/davidm-python/test_day_03.py
@@ -2,6 +2,7 @@ import re
 
 import day_03
 
+
 def test_part_1():
     data = [
         '#1 @ 1,3: 4x4',
@@ -11,6 +12,7 @@ def test_part_1():
     data = [tuple(map(int, re.findall('\d+', input))) for input in data]
     expected_result = 4
     assert day_03.part_1(data) == expected_result
+
 
 def test_part_2():
     data = [
